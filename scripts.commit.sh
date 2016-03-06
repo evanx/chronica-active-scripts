04e5e15f84c787678a8c19d68aaadc975816d1e9
Evan Summers <evan.summers@gmail.com>
2016-03-06 15:09:40 +0200

    sync

--- scripts.commit.sh
+++ scripts.commit.sh
@@ -4,13 +4,26 @@ set -u
 message="$1"
 echo "message: $message"
 
+c0remote_evanx() {
+  git remote set-url origin https://github.com/evanx/chronica-scripts
+}
+
+c0remote_http() {
+  git remote set-url origin git@github.com:evanx/chronica-scripts.git
+}
+
+c0commit() {
+  git add -A 
+  git commit -m "$message"
+  git push 
+}
+
 if [ ! -f commit.sh ] 
 then
   echo 'error'
   exit 1
 else
-  git add -A 
-  git commit -m "$message"
-  git push 
+  c0remote_evanx
+  c0commit 
 fi
 
